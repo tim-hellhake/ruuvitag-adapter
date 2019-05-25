3b03683672c4cfdd3e617f0a67b76422dadbcf19
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-05-25 23:49:35 +0200

    Release initial version 0.1.0

---
+++ README.md
@@ -0,0 +1,13 @@
+# RuuviTag Adapter
+
+[![Build Status](https://travis-ci.org/tim-hellhake/ruuvitag-adapter.svg?branch=master)](https://travis-ci.org/tim-hellhake/ruuvitag-adapter)
+[![dependencies](https://david-dm.org/tim-hellhake/ruuvitag-adapter.svg)](https://david-dm.org/tim-hellhake/ruuvitag-adapter)
+[![devDependencies](https://david-dm.org/tim-hellhake/ruuvitag-adapter/dev-status.svg)](https://david-dm.org/tim-hellhake/ruuvitag-adapter?type=dev)
+[![optionalDependencies](https://david-dm.org/tim-hellhake/ruuvitag-adapter/optional-status.svg)](https://david-dm.org/tim-hellhake/ruuvitag-adapter?type=optional)
+[![license](https://img.shields.io/badge/license-MPL--2.0-blue.svg)](LICENSE)
+
+Connect your RuuviTags (bluetooth environmental sensor).
+
+Configuration:
+- Go to 'Add Thing'
+- A new RuuviTag should appear
---
+++ ruuvitag-adapter.js
@@ -0,0 +1,91 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ */
+
+'use strict';
+
+const noble = require('@abandonware/noble');
+
+const {
+  Adapter,
+  Device,
+  Property
+} = require('gateway-addon');
+
+class RuuviTag extends Device {
+  constructor(adapter, manifest, id) {
+    super(adapter, `${RuuviTag.name}-${id}`);
+    this['@context'] = 'https://iot.mozilla.org/schemas/';
+    this['@type'] = ['TemperatureSensor'];
+    this.name = manifest.display_name;
+    this.description = manifest.description;
+
+    this.addProperty({
+      type: 'number',
+      '@type': 'TemperatureProperty',
+      minimum: -127.99,
+      maximum: 127.99,
+      unit: 'degree celsius',
+      title: 'temperature',
+      description: 'The ambient temperature',
+      readOnly: true
+    });
+  }
+
+  addProperty(description) {
+    const property = new Property(this, description.title, description);
+    this.properties.set(description.title, property);
+  }
+
+  setData(manufacturerData) {
+    const digits = manufacturerData.readUInt8(5) / 100;
+    const binary = manufacturerData.readUInt8(4);
+    const value = binary & 0x7f;
+    const sign = binary & 0x80 ? -1 : 1;
+    const temperature = sign * value + digits;
+
+    const property = this.properties.get('temperature');
+    property.setCachedValue(temperature);
+    this.notifyPropertyChanged(property);
+  }
+}
+
+class RuuviTagAdapter extends Adapter {
+  constructor(addonManager, manifest) {
+    super(addonManager, RuuviTagAdapter.name, manifest.name);
+    this.pollInterval = manifest.moziot.config.pollInterval;
+    this.knownDevices = {};
+    addonManager.addAdapter(this);
+
+    noble.on('stateChange', (state) => {
+      console.log('Noble adapter is %s', state);
+
+      if (state === 'poweredOn') {
+        console.log('Start scanning for devices');
+        noble.startScanning([], true);
+      }
+    });
+
+    noble.on('discover', (peripheral) => {
+      const manufacturerData = peripheral.advertisement.manufacturerData;
+
+      if (manufacturerData && manufacturerData.readUInt16LE(0) === 0x0499) {
+        const id = peripheral.id;
+        let knownDevice = this.knownDevices[id];
+
+        if (!knownDevice) {
+          console.log(`Detected new RuuviTag with id ${id}`);
+          knownDevice = new RuuviTag(this, manifest, id);
+          this.handleDeviceAdded(knownDevice);
+          this.knownDevices[id] = knownDevice;
+        }
+
+        knownDevice.setData(manufacturerData);
+      }
+    });
+  }
+}
+
+module.exports = RuuviTagAdapter;
